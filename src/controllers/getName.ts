390dce37425841c8f3d9088deda290d22524d51b
João <jvdvandrade@outlook.pt>
2024-12-20 01:09:22 -0300

    comentário deletado

--- src/controllers/getName.ts
+++ src/controllers/getName.ts
@@ -19,28 +19,3 @@ export default class NameController {
         }
     }
 }
-
-
-
-// export const obterNome = async (req: Request, res: Response): Promise<void> => {
-//     const { resposta } = req.body;
-
-//     try {
-//         if (resposta.toLowerCase() === 'sim') {
-//             const nomeDriver = await prisma.$queryRaw<any[]>`
-//                 SELECT 
-//                     (SELECT prefixo FROM "decomposedNames" ORDER BY RANDOM() LIMIT 1) AS pre,
-//                     (SELECT fill FROM "decomposedNames" ORDER BY RANDOM() LIMIT 1) AS mid,
-//                     (SELECT sufixo FROM "decomposedNames" ORDER BY RANDOM() LIMIT 1) AS su
-//             `;
-//             const nomeConcatenado = nomeDriver[0].pre + nomeDriver[0].mid + nomeDriver[0].su;
-//             res.status(200).json(nomeConcatenado);
-//         } else {
-//             throw new Error('Resposta não válida!');
-//         }
-
-//     } catch (error) {
-//         console.error(error);
-//         res.status(400).json({ message:'Algo deu errado!' });
-//     }
-// };
